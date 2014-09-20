f2c38960517e30233a1b26bea89754971743500c
Kunal Verma <kunal-verma@live.in>
2014-09-20 22:54:08 +0530

    Simple WP8 based run tracking application

---
+++ WP8Runner/MainPage.xaml.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.Phone.Controls;
+using Microsoft.Phone.Maps.Controls;
+using Microsoft.Phone.Shell;
+using NExtra.Geo;
+using System;
+using System.Device.Location;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Windows.Devices.Geolocation;
+
+namespace WP8Runner
+{
+  public partial class MainPage : PhoneApplicationPage
+  {
+    private GeoCoordinateWatcher _watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+    private MapPolyline _line;
+    private DispatcherTimer _timer = new DispatcherTimer();
+    private long _startTime;
+
+    public MainPage()
+    {
+      InitializeComponent();
+
+      // create a line which illustrates the run
+      _line = new MapPolyline();
+      _line.StrokeColor = Colors.Red;
+      _line.StrokeThickness = 5;
+      Map.MapElements.Add(_line);
+
+      _watcher.PositionChanged += Watcher_PositionChanged;
+
+      _timer.Interval = TimeSpan.FromSeconds(1);
+      _timer.Tick += Timer_Tick;
+      
+    }
+
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+      TimeSpan runTime = TimeSpan.FromMilliseconds(System.Environment.TickCount - _startTime);
+      timeLabel.Text = runTime.ToString(@"hh\:mm\:ss");
+    }
+
+
+    private async void UpdateMap()
+    {
+        Geolocator geolocator = new Geolocator();
+        geolocator.DesiredAccuracyInMeters = 50;
+
+        //SetProgressIndicator(true);
+        //SystemTray.ProgressIndicator.Text = "Getting GPS Location";
+
+        try
+        {
+            Geoposition position =
+                await geolocator.GetGeopositionAsync(
+                TimeSpan.FromMinutes(1),
+                TimeSpan.FromSeconds(30));
+
+            //SystemTray.ProgressIndicator.Text = "Acquired";
+
+            var gpsCoorCenter =
+                new GeoCoordinate(
+                    position.Coordinate.Latitude,
+                    position.Coordinate.Longitude);
+
+            Map.Center = gpsCoorCenter;
+            
+
+            //SetProgressIndicator(false);
+        }
+        catch (UnauthorizedAccessException)
+        {
+            MessageBox.Show("Location is disable in phone settings.");
+            
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+
+    private void StartButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            if (_timer.IsEnabled)
+            {
+                _watcher.Stop();
+                _timer.Stop();
+                StartButton.Content = "Start";
+            }
+            else
+            {
+                _watcher.Start();
+                _timer.Start();
+                _startTime = System.Environment.TickCount;
+                StartButton.Content = "Stop";
+            }
+        }
+
+        catch (Exception e1)
+        {
+
+        }
+    }
+
+    //ID_CAP_LOCATION
+    private double _kilometres;
+    private long _previousPositionChangeTick;
+
+    private void Watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+    {
+      var coord = new GeoCoordinate(e.Position.Location.Latitude, e.Position.Location.Longitude);
+
+      if (_line.Path.Count > 0)
+      {
+          try
+          {
+
+              var previousPoint = _line.Path.Last();
+              var distance = coord.GetDistanceTo(previousPoint);
+              var millisPerKilometer = (1000.0 / distance) * (System.Environment.TickCount - _previousPositionChangeTick);
+              _kilometres += distance / 1000.0;
+
+              paceLabel.Text = TimeSpan.FromMilliseconds(millisPerKilometer).ToString(@"mm\:ss");
+              distanceLabel.Text = string.Format("{0:f2} km", _kilometres);
+              caloriesLabel.Text = string.Format("{0:f0}", _kilometres * 65);
+
+              PositionHandler handler = new PositionHandler();
+              var heading = handler.CalculateBearing(new Position(previousPoint), new Position(coord));
+              Map.SetView(coord, Map.ZoomLevel, heading, MapAnimationKind.Parabolic);
+          }
+          catch (Exception eq)
+          {
+
+          }
+        ShellTile.ActiveTiles.First().Update(new IconicTileData()
+        {
+            Title = "Run Fatty Run!",
+          WideContent1 = string.Format("{0:f2} km", _kilometres),
+          WideContent2 = string.Format("{0:f0} calories", _kilometres * 65),
+        });
+      }
+      else
+      {
+        Map.Center = coord;
+      }
+
+      _line.Path.Add(coord);
+      _previousPositionChangeTick = System.Environment.TickCount;
+    }
+
+    private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+    {
+        UpdateMap();
+    }
+  }
+}
